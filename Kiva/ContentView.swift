2d2495f8b7faebbb2e3c172e489e4ba5b975b0c0
Kenneth Jackson <kjackson@connectionsis.com>
2020-06-12 12:36:31 -0500

    Initial Commit

---
+++ Kiva/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Kiva
+//
+//  Created by Kenneth on 6/12/20.
+//  Copyright © 2020 Connections.Digital. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
